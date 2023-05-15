1beed745357a844b713f314758988a5f4a844b61
amir naeeni <34509463+amirda9@users.noreply.github.com>
2023-05-14 20:52:25 -0400

    weight init add

--- cycleGan.py
+++ cycleGan.py
@@ -29,6 +29,10 @@ class GeneratorMeas(nn.Module):
             nn.Linear(256, 608)
         )
         
+        # weight initialization
+        self.encoder.apply(weights_init)
+        self.decoder.apply(weights_init)
+        
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
@@ -52,6 +56,10 @@ class GeneratorState(nn.Module):
             nn.Linear(256, 236)
         )
         
+        # weight initialization
+        self.encoder.apply(weights_init)
+        self.decoder.apply(weights_init)
+        
     def forward(self, x):
         encoded = self.encoder(x)
         decoded = self.decoder(encoded)
@@ -68,9 +76,14 @@ class DiscriminatorMeas(nn.Module):
             nn.ReLU(),
             nn.Linear(256, 128),
             nn.ReLU(),
+            nn.Linear(128, 64),
+            nn.ReLU(),
             nn.Linear(128, 1),
             nn.Sigmoid()
         )
+        # weight initialization
+        self.net.apply(weights_init)
+        
         
     def forward(self, x):
         return self.net(x)
@@ -80,7 +93,9 @@ class DiscriminatorState(nn.Module):
     def __init__(self):
         super(DiscriminatorState, self).__init__()
         self.net = nn.Sequential(
-            nn.Linear(236, 128),
+            nn.Linear(236, 256),
+            nn.ReLU(),
+            nn.Linear(256, 128),
             nn.ReLU(),
             nn.Linear(128, 64),
             nn.ReLU(),
@@ -88,8 +103,25 @@ class DiscriminatorState(nn.Module):
             nn.Sigmoid()
         )
         
+        # weight initialization
+        self.net.apply(weights_init)
+        
     def forward(self, x):
         return self.net(x)
+
+# weight initialization
+def weights_init(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_.weight.data(m.weight)
+        m.bias.data.fill_(0.01)
+    elif isinstance(m, nn.Conv2d):
+        nn.init.xavier_uniform_.weight.data(m.weight)
+        m.bias.data.fill_(0.01)
+    elif isinstance(m, nn.BatchNorm2d):
+        m.weight.data.fill_(1)
+        m.bias.data.zero_()
+    else:
+        pass
 
 # Define a custom dataset class
 class CustomDataset(Dataset):
@@ -124,13 +156,15 @@ train_dataloader = DataLoader(train_dataset, batch_size=64, shuffle=True)
 
 # Define the loss functions binary cross entropy and MSE
 adversarial_loss = nn.BCELoss()
-cycle_consistency_loss = nn.MSELoss()
+cycle_consistency_loss = nn.L1Loss()
 
 # Define the generators and discriminators
 G_state2measurement = GeneratorMeas()
 G_measurement2state = GeneratorState()
 D_measurement = DiscriminatorMeas()
 D_state = DiscriminatorState()
+
+
 
 # configs
 learning_rate = 0.01
@@ -162,12 +196,16 @@ l_D_state = []
 l_cycle = []
 l_total = []
 for epoch in range(num_epochs):
-    if epoch <100:
-        lambda_cycle = 0.1
-    elif epoch < 500:
-        lambda_cycle = 0.2
-    else: 
-        lambda_cycle = 0.5
+    D_measurement.train()
+    D_state.train()
+    G_state2measurement.train()
+    G_measurement2state.train()
+    
+    if epoch < 100:
+        lambda_cycle = 0
+    else:
+        lambda_cycle = 1
+        
     l_G_measurement_raw = []
     l_G_state_raw = []
     l_D_measurement_raw = []
@@ -175,6 +213,7 @@ for epoch in range(num_epochs):
     l_cycle_raw = []
     l_total_raw = []
     for i, (state_data, measurement_data) in enumerate(train_dataloader):
+        
         
         # Train the discriminators
         D_measurement.zero_grad()
@@ -209,50 +248,49 @@ for epoch in range(num_epochs):
         optimizer_D_measurement.step()
         optimizer_D_state.step()
 
-        
-        # Train the generators
-        G_state2measurement.zero_grad()
-        G_measurement2state.zero_grad()
-        
-        # Forward pass
-        fake_measurement = G_state2measurement(real_state)
-        fake_state = G_measurement2state(real_measurement)
-        
-        output_fake_measurement = D_measurement(fake_measurement)
-        output_fake_state = D_state(fake_state)
-        
-        # Adversarial loss
-        loss_G_state2measurement = adversarial_loss(output_fake_measurement, label_real)
-        loss_G_measurement2state = adversarial_loss(output_fake_state, label_real)
-        
-        # Cycle consistency loss
-        reconstructed_measurement = G_state2measurement(fake_state)
-        reconstructed_state = G_measurement2state(fake_measurement)
-        
-        loss_cycle_state = cycle_consistency_loss(reconstructed_state, real_state)
-        loss_cycle_measurement = cycle_consistency_loss(reconstructed_measurement, real_measurement)
-        
-        loss_cycle = loss_cycle_state + loss_cycle_measurement
-        
-        # Total loss
-        loss_G = loss_G_state2measurement + loss_G_measurement2state + lambda_cycle * loss_cycle
-        loss_G.backward()
-        
-        optimizer_G.step()
+        if epoch % 5 == 0:
+            # Print losses
+            if i % 100 == 0:
+                print('[Epoch %d/%d] [Batch %d/%d] [D loss: %.4f] [G loss: %.4f] [Cycle loss: %.4f]' %
+                    (epoch, num_epochs, i, len(train_dataloader),
+                    (loss_D_measurement + loss_D_state).item(), loss_G.item(), loss_cycle.item()))
+            # Train the generators
+            G_state2measurement.zero_grad()
+            G_measurement2state.zero_grad()
+            
+            # Forward pass
+            fake_measurement = G_state2measurement(real_state)
+            fake_state = G_measurement2state(real_measurement)
+            
+            output_fake_measurement = D_measurement(fake_measurement)
+            output_fake_state = D_state(fake_state)
+            
+            # Adversarial loss
+            loss_G_state2measurement = adversarial_loss(output_fake_measurement, label_real)
+            loss_G_measurement2state = adversarial_loss(output_fake_state, label_real)
+            
+            # Cycle consistency loss
+            reconstructed_measurement = G_state2measurement(fake_state)
+            reconstructed_state = G_measurement2state(fake_measurement)
+            
+            loss_cycle_state = cycle_consistency_loss(reconstructed_state, real_state)
+            loss_cycle_measurement = cycle_consistency_loss(reconstructed_measurement, real_measurement)
+            
+            loss_cycle = loss_cycle_state + loss_cycle_measurement
+            
+            # Total loss
+            loss_G = loss_G_state2measurement + loss_G_measurement2state + lambda_cycle * loss_cycle
+            loss_G.backward()
+            
+            optimizer_G.step()
 
-        # save the loss
-        l_G_measurement_raw.append(loss_G_measurement2state.detach().cpu().numpy())
-        l_G_state_raw.append(loss_G_state2measurement.detach().cpu().numpy())
-        l_cycle_raw.append(loss_cycle.detach().cpu().numpy())
-        l_total_raw.append(loss_G.detach().cpu().numpy())
+            # save the loss
+            l_G_measurement_raw.append(loss_G_measurement2state.detach().cpu().numpy())
+            l_G_state_raw.append(loss_G_state2measurement.detach().cpu().numpy())
+            l_cycle_raw.append(loss_cycle.detach().cpu().numpy())
+            l_total_raw.append(loss_G.detach().cpu().numpy())
         l_D_measurement_raw.append(loss_real_measurement.detach().cpu().numpy() + loss_fake_measurement.detach().cpu().numpy())
         l_D_state_raw.append(loss_real_state.detach().cpu().numpy() + loss_fake_state.detach().cpu().numpy())
-        
-        # Print losses
-        if i % 100 == 0:
-            print('[Epoch %d/%d] [Batch %d/%d] [D loss: %.4f] [G loss: %.4f] [Cycle loss: %.4f]' %
-                  (epoch, num_epochs, i, len(train_dataloader),
-                   (loss_D_measurement + loss_D_state).item(), loss_G.item(), loss_cycle.item()))
 
     l_G_measurement.append(np.mean(l_G_measurement_raw))
     l_G_state.append(np.mean(l_G_state_raw))
@@ -272,14 +310,6 @@ torch.save(G_state2measurement.state_dict(), './results/G_state2measurement_' + 
 torch.save(G_measurement2state.state_dict(), './results/G_measurement2state_' + dt_string + '.pth')
 torch.save(D_measurement.state_dict(), './results/D_measurement_' + dt_string + '.pth')
 torch.save(D_state.state_dict(), './results/D_state_' + dt_string + '.pth')
-
-# pickle.dump(l_G_measurement, open('l_G_measurement.pkl', 'wb')) 
-# pickle.dump(l_G_state, open('l_G_state.pkl', 'wb'))
-# pickle.dump(l_D_measurement, open('l_D_measurement.pkl', 'wb'))
-# pickle.dump(l_D_state, open('l_D_state.pkl', 'wb'))
-# pickle.dump(l_cycle, open('l_cycle.pkl', 'wb'))
-# pickle.dump(l_total, open('l_total.pkl', 'wb'))
-
 
 # save the plot for six losses with the date
 plt.figure()
